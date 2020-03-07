aad5f810909479f80c84171d2a0a1dce0fe01b29
Olusola Olagoroye <solaolagoroye@gmail.com>
2020-03-06 20:52:20 -0600

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,5 +3,22 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
+ 
+  def posts_index
+    @author = Author.find(params[:id])
+    @posts = @author.posts
+    render template: 'posts/index'
+  end
+  def post
+    @author = Author.find(params[:id])
+  @post = Post.find(params[:post_id])
+    render template: 'posts/show'
+  end
+ 
+  def post
+    @author = Author.find(params[:id])
+  def show
+    @author = Author.find(params[:id])
+  end
 
 end
